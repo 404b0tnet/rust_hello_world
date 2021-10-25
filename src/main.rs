597dcdc896b304e3de8c1aa55cf0f915992eb62d
404b0tnet <jsmith728.js@gmail.com>
2021-10-25 16:38:07 -0400

    created test print function for CI in Github

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let mut line = String::new();
+    println!( "Please enter your name: ");
+    std::io::stdin().read_line(&mut line).unwrap();
+    println!("{}", print_hello(line));
+}
+
+
+fn print_hello(name: String) -> String {
+    let message = "Hello ".to_owned() + &name;
+    return message;
+}
+
+
+#[test]
+fn test_build_message(){
+    let name = String::from("Jack");
+
+    assert_eq!(print_hello(name), "Hello Jack")
 }
